4f30e4b7763a8bd00816beb78826e256a7b70c13
Alan <terraplatform10@gmail.com>
2023-01-24 12:41:09 +0800

    Gen

--- Gen/Gen.cs
+++ Gen/Gen.cs
@@ -7,6 +7,93 @@ class Gen : Object
 {
     public int Execute()
     {
+        bool b;
+
+        b = this.GetFileName();
+
+
+        if (!b)
+        {
+            return 1;
+        }
+
+
+
+
+
+
+
+
+
         return 0;
+    }
+
+
+
+
+    private byte[] GetPixelData()
+    {
+        Rectangle rect;
+
+        rect = new Rectangle(Point.Empty, this.Bitmap.Size);
+
+
+        BitmapData data;
+        
+        data = this.Bitmap.LockBits(
+            rect, ImageLockMode.ReadWrite, this.Bitmap.PixelFormat
+            );
+
+
+var pixelSize = data.PixelFormat == PixelFormat.Format32bppArgb ? 4 : 3; // only works with 32 or 24 pixel-size bitmap!
+var padding = data.Stride - (data.Width * pixelSize);
+var bytes = new byte[data.Height * data.Stride];
+
+// copy the bytes from bitmap to array
+Marshal.Copy(data.Scan0, bytes, 0, bytes.Length);
+
+    }
+
+
+
+
+    private bool CreateBitmap()
+    {
+        this.Bitmap = new Bitmap(this.FileName);
+
+
+
+        return true;
+    }
+
+
+
+
+    private Bitmap Bitmap { get; set; }
+
+
+
+
+    private string FileName { get; set; }
+
+
+
+    private bool GetFileName()
+    {
+        string s;
+
+        s = Console.ReadLine();
+
+
+        if (s == null)
+        {
+            return false;
+        }
+
+
+        this.FileName = s;
+
+
+        return true;
     }
 }
--- Gen/Import.cs
+++ Gen/Import.cs
@@ -1,2 +1,7 @@
 global using Object = System.Infra.Object;
 global using ExeExe = System.Exe.Exe;
+global using Console = System.Console;
+global using Bitmap = System.Drawing.Bitmap;
+global using Rectangle = System.Drawing.Rectangle;
+global using Point = System.Drawing.Point;
+global using BitmapData = System.Drawing.Imaging.BitmapData;
